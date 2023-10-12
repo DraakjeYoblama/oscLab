a7adf86be86c540217ba347d7325fbabc5899d41
Douwe <douwe.somers@student.kuleuven.be>
2023-10-12 09:23:39 +0200

    removed duplicates

--- homework2/hellofunc.h
+++
@@ -1,4 +0,0 @@
-/*
-header file
-*/
-void myPrintHelloMake(char * who);
--- homework2/hellomain.c
+++
@@ -1,8 +0,0 @@
-#include "hellofunc.h"
-
-int main(int argc, char **argv) {
-	//call a function in another file
-	if (argc == 2) myPrintHelloMake(argv[1]);
-	else myPrintHelloMake("nobody");
-	return 0;
-}
