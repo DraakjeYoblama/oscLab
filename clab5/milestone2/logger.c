e2f289d684963cbcfd1e40d630bcc127918e70d3
Douwe Somers <douwe.somers@student.kuleuven.be>
2023-11-18 17:39:05 +0100

    Started Milestone 2

---
+++ clab5/milestone2/logger.c
@@ -0,0 +1,16 @@
+#include "logger.h"
+
+int write_to_log_process(char *msg){
+    //TODO
+    return 0;
+};
+
+int create_log_process() {
+    //TODO
+    return 0;
+};
+
+int end_log_process() {
+    //TODO
+    return 0;
+};
