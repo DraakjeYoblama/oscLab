bacceb9bd43a874785df6cc8c07ac8d745879a62
Douwe <douwe.somers@student.kuleuven.be>
2023-11-19 12:21:20 +0100

    milestone 2 started logger

--- clab5/milestone2/sensor_db.c
+++ clab5/milestone2/sensor_db.c
@@ -1,14 +1,17 @@
 #include "sensor_db.h"
 
-//TODO: check if this file doesn't cause any issues
+//TODO: use better log messages
 
 FILE * open_db(char * filename, bool append) {
     FILE* fp;
 
     if (!append) {
         fp = fopen(filename, "w+"); // replace file
+        write_to_log_process("create new csv file");
     } else {
         fp = fopen(filename, "a+"); //append to existing file
+        // this also creates a new file if none exists, not sure if that needs to change for loggin purposes
+        write_to_log_process("append to existing csv file or create new csv file");
     }
 
     return fp;
@@ -16,10 +19,12 @@ FILE * open_db(char * filename, bool append) {
 
 int insert_sensor(FILE * f, sensor_id_t id, sensor_value_t value, sensor_ts_t ts) {
     fprintf(f, "%d, %lf, %ld\n", id, value, ts);
+    write_to_log_process("sensor value added");
     return 0;
 }
 
 int close_db(FILE * f){
     fclose(f);
+    write_to_log_process("csv file closed");
     return 0;
 }
