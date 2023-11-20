960da438664688d416d2fc378a23282989aab246
Douwe <douwe.somers@student.kuleuven.be>
2023-11-20 17:29:49 +0100

    iets beter al

--- clab5/milestone2/logger.c
+++ clab5/milestone2/logger.c
@@ -8,7 +8,7 @@
 #include "logger.h"
 
 // pipes: reading end is 0, writing end is 1
-int fd1[2]; //pipe 1
+int fd1[2];
 pid_t pid;
 
 int write_to_log_process(char *msg){
@@ -17,6 +17,7 @@ int write_to_log_process(char *msg){
     if (pid>0) {
         // parent
         write(fd1[1], msg, strlen(msg)+1);
+        signal(SIGUSR1, iets); //todo handler
 
     }
 
@@ -40,20 +41,21 @@ int create_log_process() {
     //TODO: use child process and stuff
     if (pid>0) {
         // parent
-
-
-        write(fd1[1], "test", 5);
-        close(fd1[1]);
+        close(fd1[0]);
 
     } else if (pid==0) {
         //child process
+        close(fd1[1]);
         char message1[50];
-        FILE* fp = fopen("gateway.log", "a"); // replace file
+        FILE* log = fopen("gateway.log", "a"); // append file
+
+        read(fd1[0], message1, 50);
+        fprintf(log, "%s\n", message1);
 
         while (1) {
             pause();
             read(fd1[0], message1, 50);
-            fprintf(fp, "%s\n", message1);
+            fprintf(log, "%s\n", message1);
         }
 
     }
