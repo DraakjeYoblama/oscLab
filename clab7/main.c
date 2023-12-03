de2e0d9c933776d39d9835c7bf965ec0ed5041f1
Douwe Somers <douwe.somers@student.kuleuven.be>
2023-12-03 11:39:03 +0100

    Started multithreading milestone 3

--- clab7/main.c
+++ clab7/main.c
@@ -4,11 +4,20 @@
 #include <time.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <pthread.h>
+
+#include "main.h"
 
 
 int main() {
+    pthread_t thread1, thread2;
+    int iret1, iret2;
+
+    iret1 = pthread_create(&thread1, NULL, reader, NULL);
+    iret1 = pthread_create(&thread1, NULL, reader, NULL);
+
+    // parent (writer thread)
     FILE *fp;
-    int i;
 
     // Open a file in read mode
     fp = fopen("sensor_data", "r");
@@ -18,23 +27,28 @@ int main() {
     double sensor_temperature;
     time_t starttime;
 
-    for (i = 0; i<7; i++) {
+    while (!feof(fp)) {
         // Read the content and store it inside buffer
         fread(&sensor_id, 2, 1, fp);
         fread(&sensor_temperature, 8, 1, fp);
         fread(&starttime, 8, 1, fp);
 
         // Print file content
-        printf("%d %lf %ld\n", sensor_id, sensor_temperature, starttime);
+        if (!feof(fp)) {
+            printf("%d %lf %ld\n", sensor_id, sensor_temperature, starttime);
+        } else {
+            printf("%d", 0);
+        }
+
+        usleep(10000);
     }
-
-
-
     // Close the file
     fclose(fp);
-
     return 0;
 }
 
-
-// none of this is right
+void *reader() {
+    // children (reader threads)
+    printf("I'm a child--------------------------------------\n");
+    return NULL;
+}
---
+++ clab7/main.h
@@ -0,0 +1,11 @@
+//
+// Created by douwe on 3/12/23.
+//
+
+#ifndef CLAB7_MAIN_H
+#define CLAB7_MAIN_H
+
+#endif //CLAB7_MAIN_H
+
+int main();
+void *reader();
