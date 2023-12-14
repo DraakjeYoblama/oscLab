51f6be9c683770735ee899888fe34eeff1c9d34a
Douwe Somers <douwe.somers@student.kuleuven.be>
2023-12-14 17:57:09 +0100

    It kind of works, but not really

--- clab4/main.c
+++ clab4/main.c
@@ -6,8 +6,6 @@
 #include <time.h>
 
 int main(){
-    printf("Hello World\n");
-
     FILE * map = fopen("room_sensor.map", "r");
     FILE * data = fopen("sensor_data", "rb");
 
@@ -15,6 +13,9 @@ int main(){
     if(data == NULL) return -1;
 
     datamgr_parse_sensor_files(map, data);
+
+    printf("%d, %f, %ld, %d\n", datamgr_get_room_id(15), datamgr_get_avg(15), datamgr_get_last_modified(15), datamgr_get_total_sensors());
+
 
     datamgr_free();
 
